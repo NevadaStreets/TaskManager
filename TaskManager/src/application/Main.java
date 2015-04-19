d923a63ae6cca5c44105db601e6e7adc4efab7e1
cealvarez <cealvarez@uc.cl>
2015-04-18 20:25:11 -0400

    Vistas controlador modificadas

--- TaskManager/src/application/Main.java
+++ TaskManager/src/application/Main.java
@@ -1,26 +1,65 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private Stage ps;
+	private BorderPane bv;
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/TaskView.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		this.ps = primaryStage;
+		this.ps.setTitle("Task Manager");
+		
+		initBigView();
+
+        showTaskView();
 	}
+	
+	 public void initBigView() {
+	        try {
+	            // Load root layout from fxml file.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(Main.class.getResource("../view/BigView.fxml"));
+	            bv = (BorderPane) loader.load();
+
+	            // Show the scene containing the root layout.
+	            Scene scene = new Scene(bv);
+	            ps.setScene(scene);
+	            ps.show();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    /**
+	     * Shows the person overview inside the root layout.
+	     */
+	    public void showTaskView() {
+	        try {
+	            // Load person overview.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(Main.class.getResource("../view/TaskView.fxml"));
+	            AnchorPane tv = (AnchorPane) loader.load();
+
+	            // Set person overview into the center of root layout.
+	            bv.setCenter(tv);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    public Stage getPrimaryStage() {
+	        return ps;
+	    }
 	
 	public static void main(String[] args) {
 		launch(args);
--- TaskManager/src/controller/Control.java
+++ TaskManager/src/controller/Control.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class Control {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Control implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+	private Button CProyecto;
+	
+	
+	
+	@FXML
+	private Label xd;
+	
+	public void changeText(ActionEvent event){
+		
+		
+	}
+
+	
 
 }
