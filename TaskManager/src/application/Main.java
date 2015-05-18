ecd148042bd6ddffcca6e9add98683d6f2ddf6dd
rpsaavedra <rpsaavedra@uc.cl>
2015-05-17 21:52:57 -0400

    todo

--- TaskManager/src/application/Main.java
+++ TaskManager/src/application/Main.java
@@ -12,6 +12,10 @@ import java.time.LocalDate;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+import java.time.temporal.ChronoUnit;
+>>>>>>> 016512e... Tareas no priorizadas con email
 =======
 import java.time.temporal.ChronoUnit;
 >>>>>>> 016512e... Tareas no priorizadas con email
